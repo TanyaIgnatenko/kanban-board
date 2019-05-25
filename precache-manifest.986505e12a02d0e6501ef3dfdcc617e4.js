6924997a7cedd3afd11537241af3ea20e263525c
TanyaIgnatenko <alisainy@gmail.com>
2019-05-26 00:36:04 +0700

    Updates

---
+++ precache-manifest.986505e12a02d0e6501ef3dfdcc617e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df512b0800b5192047aa7ca13b408312",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "d82a8680be87f1ffb007",
+    "url": "/kanban-board/static/css/main.17f8980b.chunk.css"
+  },
+  {
+    "revision": "b6078dedec87a8fd3464",
+    "url": "/kanban-board/static/js/2.a23e2a76.chunk.js"
+  },
+  {
+    "revision": "d82a8680be87f1ffb007",
+    "url": "/kanban-board/static/js/main.f2fcc3bc.chunk.js"
+  },
+  {
+    "revision": "2a97cbc7210d9a44b96f",
+    "url": "/kanban-board/static/js/runtime~main.8507f43b.js"
+  },
+  {
+    "revision": "e82c0deb46d38f804bbe1180dc68b2a3",
+    "url": "/kanban-board/static/media/cross-icon.e82c0deb.svg"
+  },
+  {
+    "revision": "7cad1f8528f4d62608601fa5551633d0",
+    "url": "/kanban-board/static/media/plus-icon.7cad1f85.svg"
+  }
+]);
