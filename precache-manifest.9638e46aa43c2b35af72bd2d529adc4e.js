8411b2fecda4534a37f679d43356015de5dee44b
Tanya Ignatenko <tanigna.work@gmail.com>
2023-04-05 14:23:03 +0700

    Updates

---
+++ precache-manifest.9638e46aa43c2b35af72bd2d529adc4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "92f0097fc26c5846048e",
+    "url": "/kanban-board/static/css/main.89244bd6.chunk.css"
+  },
+  {
+    "revision": "92f0097fc26c5846048e",
+    "url": "/kanban-board/static/js/main.92f0097f.chunk.js"
+  },
+  {
+    "revision": "186d2e44ab719555dc6b",
+    "url": "/kanban-board/static/js/1.186d2e44.chunk.js"
+  },
+  {
+    "revision": "96213ff335c6a139d385",
+    "url": "/kanban-board/static/js/runtime~main.96213ff3.js"
+  },
+  {
+    "revision": "5fe0db661a8f134c8ff7cb418373c469",
+    "url": "/kanban-board/static/media/cross-icon.5fe0db66.svg"
+  },
+  {
+    "revision": "39184f7780f90b0a992c7c86b30d688e",
+    "url": "/kanban-board/static/media/plus-icon.39184f77.svg"
+  },
+  {
+    "revision": "760aac81e2ff169ff2a7ac5b6e24ba5c",
+    "url": "/kanban-board/index.html"
+  }
+];
