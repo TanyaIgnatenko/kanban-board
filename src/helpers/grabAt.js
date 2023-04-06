d3ca8dc59df8f18425ce3da1aa8e60c95a84f6a5
Tanya Ignatenko <tanigna.work@gmail.com>
2023-04-06 20:52:58 +0700

    Replace realistic grab animation with the same animation for all cases

--- src/helpers/grabAt.js
+++ src/helpers/grabAt.js
@@ -1,24 +1,9 @@
-import { distance, findTriangleAngle } from './math';
-
-function grabAt(grabPoint, dimensions) {
-  const center = { x: dimensions.width / 2, y: dimensions.height / 2 };
-
-  const distanceToCenter = distance(grabPoint, center);
-  const desiredCenterPosition = {
-    x: grabPoint.x,
-    y: grabPoint.y + distanceToCenter,
-  };
-  const diff = distance(center, desiredCenterPosition);
-
-  const sign = Math.sign(center.x - grabPoint.x);
-
-  const angle =
-    sign * findTriangleAngle(distanceToCenter, distanceToCenter, diff);
-
+// It doesn't require grapPoint and dimensitions params cause it always returns the same style for a slope
+// This params were for realisticGrabAt
+function grabAt() {
   return {
-    transformOrigin: `${(grabPoint.x / dimensions.width) *
-      100}% ${(grabPoint.y / dimensions.height) * 100}%`,
-    transform: `rotate(${angle}rad)`,
+    transformOrigin: '0 0',
+    transform: 'rotate(0.087rad)',
   };
 }
 
---
+++ src/helpers/realisticGrabAt.js
@@ -0,0 +1,25 @@
+import { distance, findTriangleAngle } from './math';
+
+function realisticGrabAt(grabPoint, dimensions) {
+  const center = { x: dimensions.width / 2, y: dimensions.height / 2 };
+
+  const distanceToCenter = distance(grabPoint, center);
+  const desiredCenterPosition = {
+    x: grabPoint.x,
+    y: grabPoint.y + distanceToCenter,
+  };
+  const diff = distance(center, desiredCenterPosition);
+
+  const sign = Math.sign(center.x - grabPoint.x);
+
+  const angle =
+    sign * findTriangleAngle(distanceToCenter, distanceToCenter, diff);
+
+  return {
+    transformOrigin: `${(grabPoint.x / dimensions.width) *
+      100}% ${(grabPoint.y / dimensions.height) * 100}%`,
+    transform: `rotate(${angle}rad)`,
+  };
+}
+
+export { realisticGrabAt };
